bf66ba60830db0e35701934711e61c0dd1606f7f
Savio Fonseca <savifon1@gmail.com>
2021-03-16 20:38:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+/*
+Imagine uma lâmpada que possa ter três estados: apagada, acessa e meia-luz.
+Desenvolva um programa que instancie 3 lâmpadas. A primeira deverá ficar apagada, a segunda deverá ficar acessa e a terceira deverá ficar meia-luz.
+Dica: pode-se implementar o “estado” como um atributo String da classe. Na classe haverá três métodos: Acender(), Apagar() e AcenderMeiaLuz().
+*/
+
+class Main {
+  public static void main(String[] args) {
+    Lampada l1 = new Lampada("l1", "apagada");
+  }
+}
+
+class Lampada {
+	String nome, estado;
+
+	public Lampada() {
+		nome = "Nova Lâmpada";
+		estado = "apagada";
+	}
+	public Lampada(String n, String e) {
+		nome = n;
+		estado = e;
+	}
+
+	public static boolean Acender(Lampada l) {
+		return true;
+	}
+}
